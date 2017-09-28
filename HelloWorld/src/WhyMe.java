072dc7bdff61c7fa75ede436bb0c3d69de20a05c
mhs <mhs@MHS-Science2-02>
2017-09-27 17:11:12 -0700

    I <3 xiao sensei

---
+++ HelloWorld/src/WhyMe.java
@@ -0,0 +1,9 @@
+
+public class WhyMe {
+	public static void main(String[] args){/*Xiao sensei notice me*/
+		/*System.out.println("I love");
+		System.out.print("Xiao Sensei");
+	*/
+		System.out.print("I love\nEric Xiao");
+	}
+}

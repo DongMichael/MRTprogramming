2e6999a326560dcb17f9ca8393795b67d5c0156d
User <User@LAPTOP-2DESJ5FV>
2017-10-03 21:46:25 -0700

    idk wat this is

---
+++ Xiao Sensei is kakkoui/src/Happy.java
@@ -0,0 +1,9 @@
+
+public class Happy {/*This is da Class, which I don't have, it is a template or blueprint that describes the behavior or state of an object and its support type*/
+	public static void main(String[] args) {/*it's basically a behavior, something you study in psych :p*/
+		System.out.println("Xiao Senei is so smart,");/*Some people call him Daddy Xiao*/
+		System.out.println("buuuuuuuuut");/*the ln after print means that the next thing printed will be on a new line*/
+		System.out.print("Robert is also my Diety");/*the lack of ln  signals that the next thing printed will be on the same line*/
+		System.out.print(" ,I would follow him to the ends of the earth");
+	}
+}

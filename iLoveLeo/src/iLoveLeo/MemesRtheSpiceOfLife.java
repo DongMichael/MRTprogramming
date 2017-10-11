d706622d8893bbc8248edcbd48c86e3408b057b3
mhs <mhs@MHS-Science2-11>
2017-10-11 15:58:19 -0700

    test

---
+++ iLoveLeo/src/iLoveLeo/MemesRtheSpiceOfLife.java
@@ -0,0 +1,11 @@
+package iLoveLeo;
+
+public class MemesRtheSpiceOfLife {
+	public static void main(String[] args){
+int hi = 2;
+double hello = 2.00000000000000000000000000001;
+boolean greetings = true;
+char salutations = 'H';
+String howNiceToMeetYourAquaintance = "a" + "OOOOO";
+}
+}

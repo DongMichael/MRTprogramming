705e562a64e7e59fb3b284e845ff8f75a1262287
mhs <mhs@MHS-Science2-11>
2017-10-11 16:59:17 -0700

    10/11/17

---
+++ StatementsPractice/src/practice.java
@@ -0,0 +1,31 @@
+
+public class practice {
+public static void main(String[] args){
+		int a = 5;
+		int b = 9;
+		int c = 16;
+		if(c>b && c>a){
+				 System.out.println(c);
+		}
+		if(a>b && a>c){
+			System.out.println(a);
+		}
+		if(b>a && b>c){
+			System.out.println(b);
+		}
+		int Leo = 0;
+		while(Leo<3){
+			System.out.println("g0 pR0GrAmMiNg");
+			Leo++;
+		}
+		int Leaf = 1;
+		while(Leaf<11){
+			System.out.println(Leaf);
+		
+			Leaf++;
+		}
+		int lol= 8;
+		System.out.println("help me");
+		}
+
+}
